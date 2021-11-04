7a2c08d1be40ceedfef8a56ac2c5bce89021070f
OnlyDropped <asheburenkov@yandex.ru>
2021-11-04 22:39:27 +0300

    added mobile 768px, 1024px, fix bug

--- js/main.js
+++ js/main.js
@@ -2,12 +2,32 @@ $('.slider').slick({
     arrows: true,
     slidesToShow: 2,
     slidesToScroll: 1,
-    //autoplay: true,
-    //autoplaySpeed: 7000,
+    autoplay: true,
+    autoplaySpeed: 7000,
     prevArrow: '<button type="button" class="slick-prev"><img src="img/prev.svg" alt=""></button>',
     nextArrow: '<button type="button" class="slick-next"><img src="img/next.svg" alt=""></button>',
     draggable: false,
     responsive: [
+                        {
+        breakpoint: 1025,
+        settings: {
+            arrows: true,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            centerMode: false,
+            centerPadding: '70px'
+        }
+    },
+                  {
+        breakpoint: 769,
+        settings: {
+            arrows: true,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            centerMode: true,
+            centerPadding: '70px'
+        }
+    },
             {
         breakpoint: 426,
         settings: {
@@ -44,11 +64,31 @@ $('.review-wrapper').slick({
     arrows: true,
     slidesToShow: 4,
     slidesToScroll: 1,
-    //autoplay: true,
-    //autoplaySpeed: 7000,
+    autoplay: true,
+    autoplaySpeed: 7000,
     prevArrow: '<button type="button" class="slick-prev"><img src="img/prev-white.svg" alt=""></button>',
     nextArrow: '<button type="button" class="slick-next"><img src="img/next-white.svg" alt=""></button>',
     responsive: [
+      {
+        breakpoint: 1025,
+        settings: {
+            arrows: true,
+            slidesToShow: 2,
+            slidesToScroll: 1,
+            centerMode: true,
+            centerPadding: '20px'
+        }
+    },
+                  {
+        breakpoint: 769,
+        settings: {
+            arrows: true,
+            slidesToShow: 2,
+            slidesToScroll: 1,
+            centerMode: true,
+            centerPadding: '20px'
+        }
+    },
             {
         breakpoint: 426,
         settings: {
@@ -103,13 +143,21 @@ $('.catalog__inner').slick({
         {
             breakpoint: 1025,
             settings: {
-                settings: "unslick"
+                arrows: true,
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                centerMode: true,
+                //centerPadding: '0px'
             }
         },
         {
             breakpoint: 769,
             settings: {
-                settings: "unslick"
+                arrows: true,
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                centerMode: true,
+                centerPadding: '30px'
             }
         },
         {
